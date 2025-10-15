7e5c7f516e2da12420ca3ad6e7391015091baa78
shahriyorfe <shohriyer@gmail.com>
2025-10-15 23:34:35 +0500

    Project initialized

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex text-center">
+      <h1 className="text-bold text-red-600 text-2xl bg-amber-400 mx-auto">
+        hello{' '}
+      </h1>
+    </div>
+  )
+}
+
+export default App
